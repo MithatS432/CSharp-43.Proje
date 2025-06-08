c98b4b9e53eaf84d0c740f3a9657c44c0ac1c62c
Mithat <mithatsirmali@gmail.com>
2025-06-08 16:52:37 +0300

    Add files via upload

---
+++ LINQ (Language Integrated Query)1/LINQ (Language Integrated Query)1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ__Language_Integrated_Query_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string> { "Ankara", "İstanbul", "İzmir", "Adana", "Antalya", "Bursa" };
+
+            // 1. Temel LINQ Sorgusu (from, where, select)
+            var aIleBaslayanlar = from sehir in sehirler
+                                  where sehir.StartsWith("A")
+                                  select sehir;
+
+            Console.WriteLine("A harfi ile başlayan şehirler (query syntax):");
+            foreach (var sehir in aIleBaslayanlar)
+                Console.WriteLine(sehir);
+
+            // 2. Lambda İfadesiyle LINQ (method syntax)
+            var uzunIsimler = sehirler.Where(s => s.Length > 6).Select(s => s.ToUpper());
+
+            Console.WriteLine("\nUzun şehir isimleri (lambda syntax):");
+            foreach (var sehir in uzunIsimler)
+                Console.WriteLine(sehir);
+        }
+    }
+}
